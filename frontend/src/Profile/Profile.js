5e40467c8e6d6ae0fbbbd326a2072a6d8fccd1c4
Wesley Jones <wes.jones@me.com>
2022-11-08 12:30:19 -0500

    add loading spinner

--- frontend/src/Profile/Profile.js
+++ frontend/src/Profile/Profile.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Toolbar from '../Toolbar/Toolbar';
+import './Profile.css'
 
 function Profile(props) {
     return (
-        <div>
+        <div className='Profile'>
             <Toolbar pageSelected='Profile'/>
+            <header className='Profile-header'>
+                Stinky
+            </header>
         </div>
     );
 }
