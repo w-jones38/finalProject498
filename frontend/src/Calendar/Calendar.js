8b874e47afef6938d397ae821226d29d80cc0f5d
Tucker Overton <tuckero88@hotmail.com>
2022-11-17 16:42:38 -0500

    redirect on date click

--- frontend/src/Calendar/Calendar.js
+++ frontend/src/Calendar/Calendar.js
@@ -1,24 +1,39 @@
-
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import Calendar from "rc-year-calendar";
 import Toolbar from '../Toolbar/Toolbar';
 //mindate={new Date(1985,11,18)} maxdate={new Date(1995,12,31)}
+//this.props.history.push('/path')
+//e => console.log(e.date.toLocaleDateString())
 
-export default function App() {
-  return (
-    <div className="Calendar">
-        <div>
-            <Toolbar pageSelected="Calendar"/>
+// const ProtectedComponent = () => {
+//       return <Navigate to="/" />
+//     }
+
+function CH_Calendar(props){
+    const navigate = useNavigate();
+
+    const navToHomepage = () => {
+        navigate('/');
+    };
+
+    return (
+        <div className="Calendar">
+            <div>
+                <Toolbar pageSelected="Calendar"/>
+            </div>
+            <div>
+            <Calendar 
+            year={1985} 
+            enableRangeSelection={true}
+            minDate={new Date(1985,10,18)} 
+            maxDate={new Date(1995,11,31)}
+            onDayClick={navToHomepage}
+            />
+            </div>
+          
         </div>
-        <div>
-        <Calendar 
-        year={1985} 
-        enableRangeSelection={true}
-        minDate={new Date(1985,10,18)} 
-        maxDate={new Date(1995,11,31)}
-        onDayClick={e => console.log(e.date.toLocaleDateString())}
-        />
-        </div>
-      
-    </div>
-  );
+    );
 }
+
+export default CH_Calendar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Homepage from './Homepage/Homepage';
 import Profile from './Profile/Profile'
 import ShowAll from './ShowAll/ShowAll';
-import CalendarPage from './Calendar/Calendar';
+import CH_Calendar from './Calendar/Calendar';
 import {
   BrowserRouter,
   Routes,
@@ -19,7 +19,7 @@ root.render(
         <Route path="/" element={<Homepage />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/showAll" element={<ShowAll />} />
-        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/calendar" element={<CH_Calendar />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
