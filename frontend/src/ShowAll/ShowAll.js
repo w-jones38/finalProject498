c0600b421628feb6f0f173bef6caa0f057f18b46
Wesley Jones <wes.jones@me.com>
2022-11-16 19:27:43 -0500

    just need to fix showALl once the new endpoint is finished

--- frontend/src/ShowAll/ShowAll.js
+++ frontend/src/ShowAll/ShowAll.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ClickablePicture from '../ClickablePicture/ClickablePicture';
+import { allStorage, b64toBlob } from '../helper';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import Toolbar from "../Toolbar/Toolbar";
 import "./ShowAll.css"
@@ -10,24 +11,35 @@ function ShowAll(props) {
 
     // TODO: need a way to set data, depends on how
     // we decide to store our images
-    const [data, setData] = useState([
-        "logo2_512.png",
-        "logo2_512.png",
-        "logo2_512.png",
-        "logo2_512.png",
-        "logo2_512.png",
-        "logo2_512.png",
-        "logo2_512.png",
-        "logo2_512.png"
-    ])
-
-    console.log(data);
+    const [data, setData] = useState([])
 
     // TODO: we should have a pop up render and show the user
     // a close up of the comic they selected
     const openPicturePreview = () => {
         console.log("AHHHH")
     };
+
+    const fetchImages = async (imageUrl) => {
+        let res;
+        try {
+            res = (await fetch(imageUrl)).json();
+
+        } catch (error) {
+            console.log(error);
+            setData([]);
+            return;
+        }
+        res.then((result) => {
+            const imageBlob = b64toBlob(result.comicStripBase64);
+            const imageObjectURL = URL.createObjectURL(imageBlob);
+            setData([imageObjectURL]);
+        })
+    };
+
+    useState(() => {
+        let all = Object.keys(allStorage());
+        fetchImages("https://localhost:7144/api/CalvinStrip");
+    }, [])
 
     return (
         <div className="showAll">
--- frontend/src/Toolbar/Toolbar.js
+++ frontend/src/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BetterButton from '../BetterButton/BetterButton';
 import { useNavigate } from 'react-router-dom';
 import './Toolbar.css'
+import { allStorage } from '../helper';
 
 function Toolbar(props) {
     const navigate = useNavigate();
@@ -14,22 +15,29 @@ function Toolbar(props) {
         navigate('/profile');
     };
 
+    // TODO REMOVE TESTING
+    const addToStore = () => {
+        let rand = (Math.floor(Math.random() * 400)+1).toString();
+        localStorage.setItem(rand,rand);
+        console.log(allStorage())
+    }
+    // END TESTING
+
     return (
         <header className='Toolbar'>
             <BetterButton disabled={props.pageSelected === "Homepage"}
                 onClick={navToHomepage} text="HOME"/>
             <img className="Toolbar-image" src="header.png" />
-            {/*START TESTING STUFF*/}
+            {/*TODO REMOVE TESTING*/}
+            <button onClick={() => {
+                console.log(allStorage());
+            }}>SHOW LOCAL STORAGE</button>
             <button onClick={() => {
                 localStorage.clear();
+                console.log(allStorage())
             }}>CLEAR LOCAL STORAGE</button>
-            <button onClick={() => {
-                localStorage.setItem(
-                    (Math.floor(Math.random() * 400) + 1).toString(),
-                    true
-                );
-            }}>ADD TO RANDOM TO LOCAL STORAGE</button>
-            {/*END TESTING STUFF*/}
+            <button onClick={addToStore}>ADD RANDOM TO LOCAL STORAGE</button>
+            {/*END TESTING*/}
             <BetterButton disabled={props.pageSelected === "Profile"}
                 onClick={navToProfile} text="PROFILE" />
         </header>
