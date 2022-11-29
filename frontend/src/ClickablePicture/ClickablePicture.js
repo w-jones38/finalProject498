80ac2af37712a81ef865b484e2e7c62892734fb6
Wesley Jones <wes.jones@me.com>
2022-11-29 12:42:26 -0500

    fixed bug

--- frontend/src/ClickablePicture/ClickablePicture.js
+++ frontend/src/ClickablePicture/ClickablePicture.js
@@ -3,7 +3,9 @@ import './ClickablePicture.css'
 function ClickablePicture(props) {
     return (
         <div className="pictureContainer">
-            <img onClick={props.onClick} src={props.src} className='picture'/>
+            <img onClick={props.onClick} 
+                src={props.src} 
+                className={!props.shouldOpaque?'picture':'pictureNoOpaque'}/>
             <figcaption className='pictureCaption'>{props.text}</figcaption>
         </div>
     )
